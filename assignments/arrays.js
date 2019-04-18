8fb80a89a2a31bc430bb735e38143f2151dde100
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-04-17 20:53:44 -0400

    Fixed a flaw in arrays.js Challenge 2! Working on stretch!

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -63,40 +63,35 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
+
 console.log(inventory[32]);
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = inventory.pop();
+
+let lastCar = inventory[inventory.length-1];
 console.log(lastCar);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-let carModels = [];
 
-for (i = 0; i < inventory.length; i++) {
-    carModels.push(inventory[i].car_model);
-}
+const Models = inventory.map(x => x.car_model);
 
-
-console.log(carModels.sort());
+console.log(Models.sort());
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
-let carYears = [];
 
-for (i = 0; i < inventory.length; i++) {
-    carYears.push(inventory[i].car_year);
-}
+const years = inventory.map(x => x.car_year);
 
-console.log(carYears.sort());
+console.log(years.sort());
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
-for (i = 0; i < carYears.length; i++) {
-    if (carYears[i] < 2000) {
-        oldCars.push(carYears[i]);
+for (i = 0; i < years.length; i++) {
+    if (years[i] < 2000) {
+        oldCars.push(years[i]);
     }
 }
 
